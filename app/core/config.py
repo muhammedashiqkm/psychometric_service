edf8d125619e47e0a179595757238d7b3f3749b4
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-12-02 09:56:14 +0530

    add FRONTENT_ALLOWED_ORIGINS

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,7 @@
 import os
+from pydantic import field_validator
 from pydantic_settings import BaseSettings
+from typing import Optional, List, Union
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Psychometric Service"
@@ -17,6 +19,27 @@ class Settings(BaseSettings):
     OPENAI_MODEL_NAME: str = "gpt-4o"
     GEMINI_MODEL_NAME: str = "gemini-1.5-flash" 
     DEEPSEEK_MODEL_NAME: str = "deepseek-chat"
+
+    FRONTENT_ALLOWED_ORIGINS: Union[List[str], str] = []
+
+    BASE_URL: str = "http://localhost:8000"
+
+    @field_validator("FRONTENT_ALLOWED_ORIGINS", mode="before")
+    @classmethod
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> List[str]:
+        """
+        Parses a comma-separated string OR a JSON-formatted list.
+        """
+        if isinstance(v, str):
+            if v.strip().startswith("["):
+                import json
+                try:
+                    return json.loads(v)
+                except json.JSONDecodeError:
+                    pass
+            return [i.strip() for i in v.split(",") if i.strip()]
+        
+        return v or []
 
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api import auth, analysis
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.logging_config import configure_logging, app_logger
+from app.core.config import settings
 
 configure_logging()
 
@@ -8,6 +10,16 @@ app = FastAPI(title="Psychometric Analysis Service")
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(analysis.router, prefix="/psychometric", tags=["Analysis"])
+
+if settings.FRONTENT_ALLOWED_ORIGINS:
+  app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[str(origin) for origin in settings.FRONTENT_ALLOWED_ORIGINS], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 async def startup_event():
